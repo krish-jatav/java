e566fe528e549917c32b6f9fbb31dd4ba9f657b3
Krish <krishjatav246@gmail.com>
2024-03-28 12:28:23 +0530

    javaBasics

---
+++ JavaBasics/day1.java
@@ -0,0 +1,6 @@
+// What is Java?
+
+//Ans => Java is a high-level, object-oriented programming language developed by Sun Microsystems
+// (now owned by Oracle Corporation) in the mid-1990s. It was designed with the principle of
+// "write once, run anywhere" (WORA), meaning that Java programs can run on any device that has
+// a Java Virtual Machine (JVM) installed, regardless of the underlying hardware and software platform.
