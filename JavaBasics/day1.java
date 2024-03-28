841cec74ab4166b53aec8d61b55937e466960523
Krish <krishjatav246@gmail.com>
2024-03-28 13:13:31 +0530

    What is the JDK, JRE, and JVM

--- JavaBasics/day1.java
+++ JavaBasics/day1.java
@@ -1,22 +1,55 @@
 // What is Java?
 
-//Ans => Java is a high-level, object-oriented programming language developed by Sun Microsystems
-// (now owned by Oracle Corporation) in the mid-1990s. It was designed with the principle of
-// "write once, run anywhere" (WORA), meaning that Java programs can run on any device that has
-// a Java Virtual Machine (JVM) installed, regardless of the underlying hardware and software platform.
+// Ans => Java is a high-level, object-oriented programming language developed
+// by Sun Microsystems
+// (now owned by Oracle Corporation) in the mid-1990s. It was designed with the
+// principle of
+// "write once, run anywhere" (WORA), meaning that Java programs can run on any
+// device that has
+// a Java Virtual Machine (JVM) installed, regardless of the underlying hardware
+// and software platform.
 
 // Key features of Java include:
 
-// Platform Independence: Java programs are compiled into bytecode, which can be executed on any platform with a JVM. This allows Java applications to be highly portable.
+// Platform Independence: Java programs are compiled into bytecode, which can be
+// executed on any platform with a JVM. This allows Java applications to be
+// highly portable.
 
-// Object-Oriented: Java follows an object-oriented programming paradigm, supporting concepts such as classes, objects, inheritance, polymorphism, and encapsulation.
+// Object-Oriented: Java follows an object-oriented programming paradigm,
+// supporting concepts such as classes, objects, inheritance, polymorphism, and
+// encapsulation.
 
-// Robustness: Java has strong memory management, exception handling, and type checking mechanisms, which help in creating reliable and robust applications.
+// Robustness: Java has strong memory management, exception handling, and type
+// checking mechanisms, which help in creating reliable and robust applications.
 
-// Security: Java has built-in security features such as bytecode verification and a security manager to protect systems from malicious code.
+// Security: Java has built-in security features such as bytecode verification
+// and a security manager to protect systems from malicious code.
 
-// Multithreading: Java supports multithreading, allowing programs to perform multiple tasks concurrently, making it suitable for developing scalable and high-performance applications.
+// Multithreading: Java supports multithreading, allowing programs to perform
+// multiple tasks concurrently, making it suitable for developing scalable and
+// high-performance applications.
 
-// Rich Standard Library: Java comes with a rich set of standard libraries (Java API) that provide support for various tasks such as I/O operations, networking, GUI development, and more.
+// Rich Standard Library: Java comes with a rich set of standard libraries (Java
+// API) that provide support for various tasks such as I/O operations,
+// networking, GUI development, and more.
 
-// Automatic Garbage Collection: Java's automatic garbage collection mechanism manages memory allocation and deallocation, making memory management easier for developers.
+// Automatic Garbage Collection: Java's automatic garbage collection mechanism
+// manages memory allocation and deallocation, making memory management easier
+// for developers.
+
+// ==================================================================================================
+
+
+//QUE => What is the difference between JDK, JRE, and JVM?
+
+// Ans=>
+
+// JDK (Java Development Kit) is used for Java development and includes tools
+// needed for compiling,
+// debugging, and running Java programs.
+// JRE (Java Runtime Environment) is used for running Java applications and includes the JVM and core
+// libraries but lacks development tools.
+// JVM JVM (Java Virtual Machine) is an abstract computing machine responsible for executing Java bytecode
+// on various platforms, providing platform independence for Java programs.
+
+// ================================================================================================================
