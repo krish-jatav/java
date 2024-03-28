864a267faaf93e9492b20cf51c680f951f0e4dc0
Krish <krishjatav246@gmail.com>
2024-03-28 12:33:05 +0530

    Key features of Java

--- JavaBasics/day1.java
+++ JavaBasics/day1.java
@@ -4,3 +4,19 @@
 // (now owned by Oracle Corporation) in the mid-1990s. It was designed with the principle of
 // "write once, run anywhere" (WORA), meaning that Java programs can run on any device that has
 // a Java Virtual Machine (JVM) installed, regardless of the underlying hardware and software platform.
+
+// Key features of Java include:
+
+// Platform Independence: Java programs are compiled into bytecode, which can be executed on any platform with a JVM. This allows Java applications to be highly portable.
+
+// Object-Oriented: Java follows an object-oriented programming paradigm, supporting concepts such as classes, objects, inheritance, polymorphism, and encapsulation.
+
+// Robustness: Java has strong memory management, exception handling, and type checking mechanisms, which help in creating reliable and robust applications.
+
+// Security: Java has built-in security features such as bytecode verification and a security manager to protect systems from malicious code.
+
+// Multithreading: Java supports multithreading, allowing programs to perform multiple tasks concurrently, making it suitable for developing scalable and high-performance applications.
+
+// Rich Standard Library: Java comes with a rich set of standard libraries (Java API) that provide support for various tasks such as I/O operations, networking, GUI development, and more.
+
+// Automatic Garbage Collection: Java's automatic garbage collection mechanism manages memory allocation and deallocation, making memory management easier for developers.
