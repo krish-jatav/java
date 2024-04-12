b50cff900398700eb5a91148b9c995d76980f29d
Krish <krishjatav246@gmail.com>
2024-04-12 15:51:10 +0530

    if , else if , else program

--- JavaBasics/basics.java
+++ JavaBasics/basics.java
@@ -304,15 +304,31 @@ import java.util.Scanner;
 //   }
 // }
 
+// public class basics {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter a Number if Check Odd and Even");
+//     int n = sc.nextInt();
+//     if (n % 2 == 0) {
+//       System.out.println(+n + ", is Even Number");
+//     } else {
+//       System.out.println(+n + " is  Odd Number");
+//     }
+//   }
+// }
 public class basics {
   public static void main(String[] args) {
+
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a Number if Check Odd and Even");
-    int n = sc.nextInt();
-    if (n % 2 == 0) {
-      System.out.println(+n + ", is Even Number");
-    } else {
-      System.out.println(+n + " is  Odd Number");
+    System.out.println("Enter Your Age");
+    int Age = sc.nextInt();
+    if (Age>=18){
+      System.out.println("You are Adult ");
+    }else if(Age >= 13 && Age >= 7 ){
+      System.out.println("You are Teenager.");
+    }else{
+      System.out.println("You are Not Adult and Not Teenager");
     }
+
   }
 }
