35a0f244acf4cef5bbde18abdac9c5e268113cd3
Krish <krishjatav246@gmail.com>
2024-04-20 14:24:56 +0530

    program for odd or even num

--- function.java
+++ function.java
@@ -1,13 +1,38 @@
 import java.util.Scanner;
 
+// public class function {
+// public static void helloWorld() {
+//     System.out.println("hello world");
+//     // System.out.println("hello world");
+// }
+// public static void main(String[] args) {
+//     helloWorld();
+//     // call
+// }
+
+// }
+
+// Write a program that takes a input from the user and verify that this number is even or odd.
+
 public class function {
-public static void helloWorld() {
-    System.out.println("hello world");
-    // System.out.println("hello world");
-}
-public static void main(String[] args) {
-    helloWorld();
-    helloWorld();
-    helloWorld();
-}
+
+    static Scanner sc = new Scanner(System.in);
+    static long n = sc.nextInt();
+
+    public static void verifying() {
+        if (n % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+
+        return;
+
+    }
+
+    public static void main(String[] args) {
+
+        verifying();
+    }
+
 }
