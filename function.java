ca44dd7cc6f15087cccd93172d869e6d99d2b986
Krish <krishjatav246@gmail.com>
2024-04-23 00:29:23 +0530

    swap value a and b

--- function.java
+++ function.java
@@ -37,18 +37,45 @@ import java.util.Scanner;
 
 // }
 
+// public class function {
+
+//     public static void swap (int a , int b){
+
+//         int temp;
+//         temp = a;
+//         a=b;
+//         b= temp;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+//     public static void main(String[] args) {
+//         int a= 10;
+//         int b = 20;
+//         swap(a, b);
+//     }
+
+// }
+
+//1. Initially, a is 10 and b is 20.
+//2. When the swap method is called with a and b as arguments, the values 10 and 20 are passed to the method parameters a and b.
+//3. Inside the swap method, the values of a and b are swapped using a temporary variable temp.
+//4. After swapping, a becomes 20 and b becomes 10.
+//5. These swapped values are printed inside the swap method.
+//6. However, since Java passes arguments by value, the changes made to a and b inside the swap method do not affect the original variables a and b in the main method. So, the values of a and b remain 10 and 20 respectively in the main method.
+
 //write a program to print the sum of numbers by taking the input from the user.
 
-public class function {
+// public class function {
 
-    public static void calculate(int num1, int num2) {
-        int sum = num1 + num2;
-        System.out.println("The sum is = " + sum );
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b= sc.nextInt();
-        calculate(a, b);
-    }
-}
+//     public static void calculate(int num1, int num2) {
+//         int sum = num1 + num2;
+//         System.out.println("The sum is = " + sum );
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b= sc.nextInt();
+//         calculate(a, b);
+//     }
+// }
+
