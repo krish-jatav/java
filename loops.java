99745e952363371e7747694e0862f06a9d75d5c0
Krish <krishjatav246@gmail.com>
2024-04-27 12:43:13 +0530

    print 1 to n number by taking the n value from the user

--- loops.java
+++ loops.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 import java.util.*;
 
-
 //while loop
 //Q.print 1 to 100 number?
+
+// public class loops {
+
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while (i<=100) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+//print the values from 1 to n(by taking the n value from the user).
 
 public class loops {
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i<=100) {
-            System.out.println(i);
-            i++;
+        Scanner sc = new Scanner(System.in);
+        int num = 1;
+        System.out.println("Enter your Number");
+        int n = sc.nextInt();
+        System.out.println("---counting start---");
+        while (num<=n) {
+System.out.println(num);
+num++;
         }
+
     }
 }
-
-
