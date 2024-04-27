2936024528cbffbc4c7c6d7fe3d395491e4a268e
Krish <krishjatav246@gmail.com>
2024-04-27 12:29:08 +0530

    while loop

---
+++ loops.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+//while loop
+//Q.print 1 to 100 number?
+
+public class loops {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+
