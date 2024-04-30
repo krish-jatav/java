71f932b259e8f17d4ac419cb5313ddb3a23fbcee
Krish <krishjatav246@gmail.com>
2024-04-30 14:20:40 +0530

    0-1 tringle

--- patterns.java
+++ patterns.java
@@ -11,21 +11,48 @@ import java.util.Scanner;
 // }
 // }
 
-public class patterns {
+//QUE --Hollow rectangle
+// public class patterns {
 
-    public static void hallow_rectanglen(int Row, int Column) {
-        for (int i = 1; i <= Row; i++) {
-            for (int j = 1; j <= Column; j++) {
-                if (i == 1 || i == Row || j == 1 || j == Column) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
-    public static void main(String[] args) {
-        hallow_rectanglen(3, 6);
-    }
-}
+//     public static void hallow_rectanglen(int Row, int Column) {
+//         for (int i = 1; i <= Row; i++) {
+//             for (int j = 1; j <= Column; j++) {
+//                 if (i == 1 || i == Row || j == 1 || j == Column) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args) {
+//         hallow_rectanglen(3, 6);
+//     }
+// }
+
+// public class patterns {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 int sum = i + j;
+//                 if (sum % 2 == 0) {
+//                     System.out.print("1");
+//                 } else {
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// OUTPUT:-
+// 1
+// 01
+// 101
+// 0101
+// 10101
+// -----------------------------------------------
