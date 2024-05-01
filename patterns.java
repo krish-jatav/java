c9f66647e1a3442282b1ac31096693caad347330
Krish <krishjatav246@gmail.com>
2024-05-01 11:36:08 +0530

    butterfly pattern

--- patterns.java
+++ patterns.java
@@ -56,3 +56,58 @@ import java.util.Scanner;
 // 0101
 // 10101
 // -----------------------------------------------
+
+// butterfly pattern
+
+// public class patterns {
+
+//     public static void butterfly(int n) {
+//         // Outer loop for the top part of the butterfly
+//         for (int i = 1; i <= n; i++) {
+//             // Print stars for the left side of the butterfly
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             // Print spaces between the wings of the butterfly
+//             for (int j = 1; j <= 2 * (n - i); j++) {
+//                 System.out.print(" ");
+//             }
+//             // Print stars for the right side of the butterfly
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             // Move to the next line
+//             System.out.println();
+//         }
+
+//         // Outer loop for the bottom part of the butterfly
+//         for (int i = n - 1; i >= 1; i--) {
+//             // Print stars for the left side of the butterfly
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             // Print spaces between the wings of the butterfly
+//             for (int j = 1; j <= 2 * (n - i); j++) {
+//                 System.out.print(" ");
+//             }
+//             // Print stars for the right side of the butterfly
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             // Move to the next line
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         butterfly(4); // You can change the parameter to adjust the size of the butterfly
+//     }
+// }
+// // OUTPUT:-
+// // *      *
+// // **    **
+// // ***  ***
+// // ********
+// // ***  ***
+// // **    **
+// // *      *
