2b4aceb7d4dfc243c23d6235035b7b3519c4a44a
Krish <krishjatav246@gmail.com>
2024-05-05 19:12:52 +0530

    print array using for loop

--- array.java
+++ array.java
@@ -1,14 +1,27 @@
 import java.util.*;
 
-public class array {
-public static void main(String[] args) {
-    int[] marks = new int [3];
-    marks[0] = 77;
-    marks[1] = 87;
-    marks[2] = 90;
-    System.out.println(marks[0]);
-    System.out.println(marks[1]);
-    System.out.println(marks[2]);
+// public class array {
+// public static void main(String[] args) {
+//     int[] marks = new int [3];
+//     marks[0] = 77;
+//     marks[1] = 87;
+//     marks[2] = 90;
+//     System.out.println(marks[0]);
+//     System.out.println(marks[1]);
+//     System.out.println(marks[2]);
 
-}
+// }
+// }
+
+public class array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 77;
+        marks[1] = 87;
+        marks[2] = 90;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
 }
