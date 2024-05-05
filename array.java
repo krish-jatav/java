2bf2b47b36273ee10da857bbbc62e32b404040a3
Krish <krishjatav246@gmail.com>
2024-05-05 19:39:05 +0530

    array elements input

--- array.java
+++ array.java
@@ -13,14 +13,33 @@ import java.util.*;
 // }
 // }
 
+// public class array {
+//     public static void main(String[] args) {
+//         int[] marks = new int[3];
+//         marks[0] = 77;
+//         marks[1] = 87;
+//         marks[2] = 90;
+//         for (int i = 0; i < 3; i++) {
+//             System.out.println(marks[i]);
+//         }
+
+//     }
+// }
+
 public class array {
     public static void main(String[] args) {
-        int[] marks = new int[3];
-        marks[0] = 77;
-        marks[1] = 87;
-        marks[2] = 90;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(marks[i]);
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int nums[] = new int[size];
+        
+        // input loop
+        for (int i = 0; i < size; i++) {
+
+            nums[i] = sc.nextInt();
+        }
+        // output loop
+        for (int i = 0; i < size; i++) {
+            System.out.println(nums[i]);
         }
 
     }
