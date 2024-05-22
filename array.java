c743a1efa6152e5eb9e6ca938b5c0978a2886075
Krish <krishjatav246@gmail.com>
2024-05-22 11:59:56 +0530

    find the largest no. of Array

--- array.java
+++ array.java
@@ -51,28 +51,45 @@ import java.util.*;
 //     }
 // }
 
+// public class array {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your marks");
+//         int marks[] = new int[100];
+
+//         marks[0] = sc.nextInt(); // phy
+//         marks[1] = sc.nextInt(); // chem
+//         marks[2] = sc.nextInt();// maths
+
+//         System.out.println("phy : " + marks[0]);
+//         System.out.println("chem : " + marks[1]);
+//         System.out.println("maths : " + marks[2]);
+
+//         int sum = marks[0] + marks[1] + marks[2];
+
+//         int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+
+//         System.out.println("Total :" + sum);
+//         System.out.println("Percentage : " + percentage + "%");
+
+//     }
+// }
+
+// QUE : find the largest no. of Array [1,3,2,4,1,6]
+
+
 public class array {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your marks");
-        int marks[] = new int[100];
-
-        marks[0] = sc.nextInt(); // phy
-        marks[1] = sc.nextInt(); // chem
-        marks[2] = sc.nextInt();// maths
-
-        System.out.println("phy : " + marks[0]);
-        System.out.println("chem : " + marks[1]);
-        System.out.println("maths : " + marks[2]);
-
-        int sum = marks[0] + marks[1] + marks[2];
-
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-
-        System.out.println("Total :" + sum);
-        System.out.println("Percentage : " + percentage + "%");
-
+        int[] largest =  {1,3,6,9,2,5};
+        int arr =largest[0];
+        for (int i=0;i < largest.length;i++){
+            if(largest[i]> arr){
+                arr = largest[i];
+            }
+        }
+        System.out.println("The largest element in the array is: " + arr);
     }
 }
