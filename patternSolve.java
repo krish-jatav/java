2ef6fad8bb4387f17b8f3272913b1177b2ce4f28
Krish <krishjatav246@gmail.com>
2024-07-01 13:31:44 +0530

    Inverted Right Pyramid

--- patternSolve.java
+++ patternSolve.java
@@ -1,20 +1,43 @@
 import java.util.Scanner;
 import java.util.*;
 
-public class patternSolve {
-    static void nTringleNum(int n) {
+// public class patternSolve {
+//     // static void nTringleNum(int n) {
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++) {
-                System.out.print(i+" ");
-            }
-            System.out.println();
-        }
+//     //     for(int i = 1; i <= n; i++) {
+//     //         for(int j = 1; j <= i; j++) {
+//     //             System.out.print(i+" ");
+//     //         }
+//     //         System.out.println();
+//     //     }
 
-    }
-    public static void main(String[] args) {
-          int n = 5;
-        nTringleNum(n);
+//     // }
 
-    }
-}
+//     public static void main(String[] args) {
+//           int n = 5;
+//           nTringleNum(n);
+
+//     }
+// }
+
+
+// Inverted Right Pyramid
+
+// public class patternSolve {
+
+//     static void  rTriangle(int n){
+//         for(int i=0; i<n; i++){
+//             for(int j = n; j>i; j--){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+//     public static void main(String[] args) {
+//           int n = 5;
+//           rTriangle(n);
+
+//     }
+// }
+
