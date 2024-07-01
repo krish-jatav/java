03a2c73e6eecfdac7445dc4c0923742bdf4280c3
Krish <krishjatav246@gmail.com>
2024-07-01 14:50:04 +0530

    Inverted Numbered Right Pyramid

--- patternSolve.java
+++ patternSolve.java
@@ -41,3 +41,23 @@ import java.util.*;
 //     }
 // }
 
+// Inverted Numbered Right Pyramid
+
+public class patternSolve {
+
+    static void  rTriangle(int n){
+        for(int i=0; i<n; i++){
+            for(int j = n; j>i; j--){
+                System.out.print(n-j+1+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+          int n = 5;
+          rTriangle(n);
+
+    }
+}
+
