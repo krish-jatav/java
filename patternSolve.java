220cba14c292cb38ef8a79aaecc3c291019d006a
Krish <krishjatav246@gmail.com>
2024-07-01 16:47:18 +0530

    Star Pyramid

--- patternSolve.java
+++ patternSolve.java
@@ -25,7 +25,7 @@ import java.util.*;
 
 // public class patternSolve {
 
-//     static void  rTriangle(int n){
+//     static void  starPyramid(int n){
 //         for(int i=0; i<n; i++){
 //             for(int j = n; j>i; j--){
 //                 System.out.print("* ");
@@ -43,21 +43,54 @@ import java.util.*;
 
 // Inverted Numbered Right Pyramid
 
+// public class patternSolve {
+
+//     static void  rTriangle(int n){
+//         for(int i=1; i<=n; i++){
+//             for(int j = 1; j<=i; j++){
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+//     public static void main(String[] args) {
+//           int n = 5;
+//           rTriangle(n);
+
+//     }
+// }
+
+
+// Star Pyramid
+
 public class patternSolve {
 
-    static void  rTriangle(int n){
-        for(int i=1; i<=n; i++){
-            for(int j = 1; j<=i; j++){
-                System.out.print(j+" ");
-            }
-            System.out.println();
+    static void  starPyramid(int n){
+
+       for(int i = 0; i<n; i++){
+       //space
+        for(int j=0; j<n-1-i; j++){
+           System.out.print(" ");
         }
+        // star
+        for(int j=0; j<2*i+1; j++){
+
+            System.out.print("*");
+
+        }
+        // space
+        for(int j=0; j<n-1-i; j++){
+           System.out.print(" ");
+        }
+
+         System.out.println();
+       }
 
     }
     public static void main(String[] args) {
           int n = 5;
-          rTriangle(n);
+          starPyramid(n);
 
     }
 }
-
