8538818b03e0bf8444427730fe5337cfa2ef4bea
Krish <krishjatav246@gmail.com>
2024-07-11 18:11:59 +0530

    Inverted Star Pyramid

--- patternSolve.java
+++ patternSolve.java
@@ -64,33 +64,66 @@ import java.util.*;
 
 // Star Pyramid
 
+// public class patternSolve {
+
+//     static void  starPyramid(int n){
+
+//        for(int i = 0; i<n; i++){
+//        //space
+//         for(int j=0; j<n-1-i; j++){
+//            System.out.print(" ");
+//         }
+//         // star
+//         for(int j=0; j<2*i+1; j++){
+
+//             System.out.print("*");
+
+//         }
+//         // space
+//         for(int j=0; j<n-1-i; j++){
+//            System.out.print(" ");
+//         }
+
+//          System.out.println();
+//        }
+
+//     }
+//     public static void main(String[] args) {
+//           int n = 6;
+//           starPyramid(n);
+
+//     }
+// }
+
+// Inverted Star Pyramid
+
 public class patternSolve {
 
-    static void  starPyramid(int n){
+   static void  starPyramid(int n){
 
-       for(int i = 0; i<n; i++){
-       //space
-        for(int j=0; j<n-1-i; j++){
-           System.out.print(" ");
-        }
-        // star
-        for(int j=0; j<2*i+1; j++){
+      for(int i = 0; i<n; i++){
+      //space
+       for(int j=0; j<i; j++){
+          System.out.print(" ");
+       }
+       // star
+       for(int j=0; j<2*n-(2*i+1); j++){
 
-            System.out.print("*");
+           System.out.print("*");
 
-        }
-        // space
-        for(int j=0; j<n-1-i; j++){
-           System.out.print(" ");
-        }
-
-         System.out.println();
+       }
+       // space
+       for(int j=0; j<i; j++){
+          System.out.print(" ");
        }
 
-    }
-    public static void main(String[] args) {
-          int n = 6;
-          starPyramid(n);
+        System.out.println();
+      }
 
-    }
+   }
+   public static void main(String[] args) {
+         int n = 6;
+         starPyramid(n);
+
+   }
 }
