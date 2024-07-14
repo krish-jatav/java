080ae5262caeba93863ad0b21830dd30c72ea7cc
Krish <krishjatav246@gmail.com>
2024-07-14 16:57:01 +0530

    passing array as argument

--- array.java
+++ array.java
@@ -93,3 +93,19 @@ import java.util.*;
 //         System.out.println("The largest element in the array is: " + arr);
 //     }
 // }
+
+public class array {
+    public static void update(int marks[]) {
+             for(int i=0; i<marks.length; i++){
+                marks[i]=marks[i] + 1;
+             }
+    }
+    public static void main(String[] args) {
+        int marks[] ={97,98,99};
+        update(marks);
+        for(int i=0; i<marks.length;i++){
+            System.out.println(marks[i] + 1 + " ");
+        }
+        System.out.println();
+    }
+}
